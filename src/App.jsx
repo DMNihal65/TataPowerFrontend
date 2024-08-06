b69831b841d6fa9cb35c8edea553c671f08885bc
DMNihal65 <nihaldm65@gmail.com>
2024-08-06 15:49:54 +0530

    initalized the git repo for the frontend and add the tailwind css for the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-2xl font-bold underline">
+ Main Home Page
+ Just import the componets here 
+  </h1>
+  )
+}
+
+export default App
